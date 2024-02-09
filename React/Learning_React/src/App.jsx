84844f8e43cc9d4a81b1524b5d48dafe351e2e92
sajid <mdskhan4774@gmail.com>
2024-02-10 01:42:38 +0530

    Handling Event

--- React/Learning_React/src/App.jsx
+++ React/Learning_React/src/App.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Card from "./components/Card";
+import Song from "./components/Song";
 
 function App() {
 
   return (
     <div>
-      <Card/>
+      {/* <Card/> */}
+      <Song/>
     </div>
   );
 }
---
+++ React/Learning_React/src/components/Song.jsx
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function Song() {
+
+    const data = [
+        { name: "Lofi Mix", desc: "lorem ipsum, dolar sit amet construction adipisicing elite." },
+        { name: "Bass boom", desc: "lorem ipsum, dolar sit amet construction adipisicing elite." },
+    ];
+
+    const handleClickDownload = ()=>{alert("downloading")};
+
+    return (
+        <div className='w-full h-screen bg-zinc-300 flex flex-col gap-4 justify-center items-center'>
+            {data.map((item, index) => {
+                return (
+                    <div key={index} className='song w-90 px-3 py-2 bg-zinc-100 rounded-md'>
+                        <h3 className='font-semibold text-xl'>{item.name}</h3>
+                        <p className='text-xs mt-2 '>{item.desc}</p>
+                        <button onClick={handleClickDownload} className='px-2 py-1 bg-blue-500 text-xs text-white rounded font-semibold mt-3'>Download Now</button>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Song;
