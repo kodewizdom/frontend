5883930d9cb6a6d77894657bf0f254be141810d3
sajid <mdskhan4774@gmail.com>
2024-05-06 19:22:38 +0530

    getting data without button click

--- React/React-Js-API/src/App.jsx
+++ React/React-Js-API/src/App.jsx
@@ -1,58 +1,44 @@
+import React from "react";
 import axios from "axios";
 import { useState } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Show from "./components/Show";
 
 const App = () => {
-  const [products,setProducts]= useState([]);
-
-  const getProduct = () => {
-    const api = "https://fakestoreapi.com/products";
-
-    axios
-      .get(api)
-      .then((products) => {
-        // console.log(products);
-        setProducts(products.data)
-      })
-      .catch((err) => console.log(err));
-  };
+  
   //----------post-----------
-  const addProduct = () => {
-    const api = "https://fakestoreapi.com/products";
+  // const addProduct = () => {
+  //   const api = "https://fakestoreapi.com/products";
 
-    axios
-      .post(api,{
-        title:"new pro",
-        price:99,
-        description:"added by me",
-        image:"https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
-        category:"car",
-      })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => console.log(err));
-  };
+  //   axios
+  //     .post(api,{
+  //       title:"new pro",
+  //       price:99,
+  //       description:"added by me",
+  //       image:"https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
+  //       category:"car",
+  //     })
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
 
   return (
-    <div className="pt-[5%] pl-[5%]">
-      <button onClick={getProduct} className="rounded px-5 py-2 bg-red-300">
-        Call Product API
-      </button>
-      <br/>
-      <br/>
-      
-      <button onClick={addProduct} className="rounded px-5 py-2 bg-blue-300">
-        Save New Product
-      </button>
-
-      <br/>
-      <br/>
-      <ul className="flex flex-wrap gap-3">
-        {products.length>0?products.map(p=> <li className=" rounded-md w-1/4 p-5 bg-red-400">{p.title}</li>):<h1>Loading...</h1>}
-        
-        
-      </ul>
-
+    <div className="pt-[5%] pl-[5%]">      
+      <nav className="flex gap-5 ">
+        <Link to="/">Home</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/show">Show</Link>
+      </nav>
+      <hr/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/services" element={<Services/>}/>
+        <Route path="/show" element={<Show/>}/>
+      </Routes>
     </div>
   );
 };
---
+++ React/React-Js-API/src/components/Show.jsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const Show = () => {
+  const [products, setProducts] = useState([]);
+
+  const getProduct = () => {
+    const api = "https://fakestoreapi.com/products";
+
+    axios
+      .get(api)
+      .then((products) => {
+        // console.log(products);
+        setProducts(products.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(()=>{
+    getProduct();
+  },[])
+  return (
+    <>
+      {/* <button onClick={getProduct} className="rounded px-5 py-2 bg-red-300">
+        Call Product API
+      </button> */}
+      
+      <ul className="flex flex-wrap gap-3 pt-10">
+        {products.length > 0 ? (
+          products.map((p) => (
+            <li key={p.id} className=" rounded-md w-1/4 p-5 bg-red-400">{p.title}</li>
+          ))
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </ul>
+    </>
+  );
+};
+
+export default Show;
